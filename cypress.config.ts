28974cb9d1dbf4d2f5f619e1e3cea247d5e82335
muriukialex <alexmuriukimaina254@gmail.com>
2024-02-16 15:38:56 +0300

    testing workflow

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,25 @@
 import { defineConfig } from "cypress"
+import jsonServer from "json-server"
 
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("before:run", () => {
+        const server = jsonServer.create()
+        const router = jsonServer.router(
+          "cypress/fixtures/empty-user-labs.json",
+        )
+        const middlewares = jsonServer.defaults()
+
+        server.use(middlewares)
+        server.use(router)
+
+        server.listen(3000, () => {
+          console.log("JSON Server is running")
+        })
+      })
     },
   },
 })
