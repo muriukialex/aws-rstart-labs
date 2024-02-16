7407a3b6d268a4c7d0f03efa8e61ed6babc1c73d
muriukialex <alexmuriukimaina254@gmail.com>
2024-02-16 19:22:25 +0300

    update metadata config

--- lib/metadata-options.ts
+++ lib/metadata-options.ts
@@ -39,13 +39,13 @@ export const metaDataOptions: Metadata = {
     },
   },
   robots: {
-    index: false,
+    index: true,
     follow: true,
     nocache: true,
     googleBot: {
       index: true,
-      follow: false,
-      noimageindex: true,
+      follow: true,
+      noimageindex: false,
       "max-video-preview": -1,
       "max-image-preview": "large",
       "max-snippet": -1,
